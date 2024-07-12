4a63874423a28bca791dd0ddfc0a1d3254d18a04
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-07-12 17:00:39 +0200

    feat: add embeddin endpoint

--- src/main/java/com/watsonx/ai/WatsonxController.java
+++ src/main/java/com/watsonx/ai/WatsonxController.java
@@ -1,44 +1,68 @@
 package com.watsonx.ai;
 
-
-import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.watsonx.WatsonxAiChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.embedding.Embedding;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.watsonx.WatsonxAiChatModel;
 import org.springframework.ai.watsonx.WatsonxAiChatOptions;
+import org.springframework.ai.watsonx.WatsonxAiEmbeddingModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1")
 public class WatsonxController {
 
-    private final WatsonxAiChatClient chat;
+    private final WatsonxAiChatModel chat;
+    private final WatsonxAiEmbeddingModel embedding;
+
+    private final String stringTemplate = """
+        <|system|>
+        You are Granite Chat, an AI language model developed by IBM.
+        You are a cautious assistant. You carefully follow instructions.
+        You are helpful and harmless and you follow ethical guidelines and promote positive behavior.
+        <|user|>
+        {input}
+        <|assistant|>
+        
+        """.stripIndent();
+
+    private final PromptTemplate template = new PromptTemplate(stringTemplate);
 
     @Autowired
-    public WatsonxController(WatsonxAiChatClient chat) {
+    public WatsonxController(WatsonxAiChatModel chat, WatsonxAiEmbeddingModel embedding) {
         this.chat = chat;
+        this.embedding = embedding;
     }
 
-    @GetMapping("/hi")
-    public String sayHi() {
-        var options = WatsonxAiChatOptions.builder().withRandomSeed(1).withModel("google/flan-ul2").withDecodingMethod("sample").build();
-        var prompt = new Prompt(new SystemMessage("say hi"), options);
-        var results = this.chat.call(prompt);
-        return results.getResult().getOutput().getContent();
+    @GetMapping("/text")
+    public ResponseEntity<String> chat(@RequestParam String input) {
+        Prompt prompt = this.template.create(Map.of("input", input));
+        ChatResponse genAiResponse = this.chat.call(prompt);
+        return ResponseEntity.ok(genAiResponse.getResult().getOutput().getContent());
     }
 
-    @GetMapping(path = "/hi/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> sayHiStream() {
-        var options = WatsonxAiChatOptions.builder().withRandomSeed(1).withModel("google/flan-ul2").withDecodingMethod("sample").build();
-        var prompt = new Prompt(new SystemMessage("say hi"), options);
-        var results = this.chat.stream(prompt);
+    @GetMapping(path = "/text/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> chatStream(@RequestParam String input) {
+        Prompt prompt = this.template.create(Map.of("input", input));
+        Flux<ChatResponse> genAiStreaming = this.chat.stream(prompt);
+
         SseEmitter emitter = new SseEmitter();
 
-        results.subscribe(
+        genAiStreaming.subscribe(
                 data -> {
                     try {
                         emitter.send(data.getResult().getOutput().getContent(), MediaType.TEXT_PLAIN);
@@ -51,5 +75,11 @@ public class WatsonxController {
         );
 
         return new ResponseEntity<>(emitter, HttpStatus.OK);
+    }
+
+    @GetMapping("/embedding")
+    public ResponseEntity<Embedding> embedding(@RequestParam String text) {
+        EmbeddingResponse response = this.embedding.embedForResponse(List.of(text));
+        return ResponseEntity.ok(response.getResult());
     }
 }
